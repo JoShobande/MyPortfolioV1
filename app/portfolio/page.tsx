7f1fa96e872f6f0c30737a93aa2a83d54cd28e6e
Josephine <shobande.josephin@gmail.com>
2024-04-03 10:17:02 +0100

    added projects

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,67 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import projects from '../../projects.json'
 
 
 const Portfolio = () => {
+
     return(
         <section className='mt-[30px]'>
             <h1 className='text-[32px] lg:text-[42px]'>Project Portfolio</h1>
             <div className='lg:flex flex-wrap mt-[40px] justify-between'>
-                <div className='border border-[#66CCFF] p-[20px] rounded-[8px] mb-[20px]'>
-                    <div className='bg-white lg:w-[300px] h-[200px] rounded-[16px] mb-[10px]'>
+                {
+                    projects.map((project, index)=>{
+                        return(
+                            <div className='border border-[#66CCFF] p-[20px] rounded-[8px] mb-[20px]' key={index}>
+                                <div className='bg-[white] lg:w-[300px] h-[200px] rounded-[16px] mb-[10px] relative'>
+                                    <Image
+                                        src={project.image}
+                                        alt='project1'
+                                        layout='fill'
+                                        objectFit='contain'
 
-                    </div>
-                    <div className='w-[300px]'>
-                        <p className='mb-[10px] text-[16px]'>Ify's Kitchen</p>
-                        <p className='mb-[10px] text-[14px]'>Ify’s kitchen is a videos streaming sites that connect chefs and students. Offline videos and streaming classes and reciepies</p>
-                        <div className='flex gap-x-[10px]'>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>React</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>Typescript</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>SCSS</p>
-                        </div>
-                    </div>  
-                    <div className='flex flex-col items-center mt-[20px]'>
-                        <button className='bg-[white] text-[#06070A] text-[16px] font-[700] rounded-t-[20px] rounded-br-[20px] w-[250px] h-[50px]'>
-                            View Live
-                        </button>
-                    </div>
-                </div>
-                <div className='border border-[#66CCFF] p-[20px] rounded-[8px] mb-[20px]'>
-                    <div className='bg-white lg:w-[300px] h-[200px] rounded-[16px] mb-[10px]'>
-
-                    </div>
-                    <div className='w-[300px]'>
-                        <p className='mb-[10px] text-[16px]'>Ify's Kitchen</p>
-                        <p className='mb-[10px] text-[14px]'>Ify’s kitchen is a videos streaming sites that connect chefs and students. Offline videos and streaming classes and reciepies</p>
-                        <div className='flex gap-x-[10px]'>
-                            <p className='bg-white rounded-[10px] text-[#101319] text-[12px] p-[10px]'>React</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] text-[12px] p-[10px]'>Typescript</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] text-[12px] p-[10px]'>SCSS</p>
-                        </div>
-                    </div> 
-                    <div className='flex flex-col items-center mt-[20px]'>
-                        <button className='bg-[white] text-[#06070A] text-[16px] font-[700]  rounded-t-[20px] rounded-br-[20px] w-[250px] h-[50px]'>
-                            View Live
-                        </button>
-                    </div>
-                </div>
-                <div className='border border-[#66CCFF] p-[20px] rounded-[8px] mb-[20px]'>
-                    <div className='bg-white lg:w-[300px] h-[200px] rounded-[16px] mb-[10px]'>
-
-                    </div>
-                    <div className='w-[300px]'>
-                        <p className='mb-[10px] text-[16px]'>Ify's Kitchen</p>
-                        <p className='mb-[10px] text-[14px]'>Ify’s kitchen is a videos streaming sites that connect chefs and students. Offline videos and streaming classes and reciepies</p>
-                        <div className='flex gap-x-[10px]'>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>React</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>Typescript</p>
-                            <p className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'>SCSS</p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col items-center mt-[20px]'>
-                        <button className='bg-[white] text-[#06070A] text-[16px] font-[700]  rounded-t-[20px] rounded-br-[20px] w-[250px] h-[50px]'>
-                            View Live
-                        </button>
-                    </div>
-                </div>
+                                    />
+                                </div>
+                                <div className='w-[300px]'>
+                                    <p className='mb-[10px] text-[16px]'>{project.name}</p>
+                                    <p className='mb-[10px] text-[14px]'>
+                                         {project.description}
+                                    </p>    
+                                    <div className='flex gap-x-[10px]'>
+                                        {
+                                            project.technology?.map((item, index)=>{
+                                                return(
+                                                    <p 
+                                                        key={index}
+                                                        className='bg-white rounded-[10px] text-[#101319] p-[10px] text-[12px]'> 
+                                                        {item}
+                                                    </p>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>  
+                                <Link href={`${project.link}`} target='_blank'  className='flex flex-col items-center mt-[20px]'>
+                                    <button className='bg-[white] text-[#06070A] text-[16px] font-[700] rounded-t-[20px] rounded-br-[20px] w-[250px] h-[50px]'>
+                                        View Live
+                                    </button>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
     )
